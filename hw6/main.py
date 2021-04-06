5bc83659dcf6ae3f8ca96199bde234cd27811a10
sidijju <sidijju@gmail.com>
2021-04-06 13:12:39 -0600

    bugfix

--- hw6/main.py
+++ hw6/main.py
@@ -7,6 +7,7 @@ from torchsummary import summary
 from torch.utils.data.dataset import Dataset  # For custom datasets
 from data import Data
 from rotnet import RotNet
+import numpy as np
 import time
 import shutil
 import yaml
@@ -27,14 +28,28 @@ config = yaml.load(open(args.config, 'r'), Loader=yaml.FullLoader)
 
 def train(train_loader, model, criterion, optimizer, epoch):
     model.train()
+    correct = 0
+    total = 0
     for i, (input, target) in enumerate(train_loader):
         optimizer.zero_grad()
         outputs = model(input)
+        predicted = torch.argmax(outputs, 1)
         loss = criterion(outputs, target)
+
+        for index in range(len(input)):
+            if predicted[index] == target[index]:
+                correct += 1
+            total += 1
+
+        if i % 100 == 0:
+            print("loss: ", loss.item())
+            print("label: ", target[0].item())
+            print("predicted: ", predicted[0].item())
+
         loss.backward()
         optimizer.step()
-        print("loss: ", loss[0])
-    return loss[0]
+
+    return loss.item(), correct/total
 
 def validate(val_loader, model, criterion):
     model.eval()
@@ -42,20 +57,18 @@ def validate(val_loader, model, criterion):
     total = 0
     for i, (input, target) in enumerate(val_loader):
         outputs = model(input)
-        predicted = torch.argmax(outputs)
-        labels = torch.argmax(target)
+        predicted = torch.argmax(outputs, 1)
         loss = criterion(outputs, target)
-        for i in range(len(input)):
-            if predicted[i] == labels[i]:
+        for index in range(len(input)):
+            if predicted[index] == target[index]:
                 correct += 1
             total += 1
-            if i % 128 == 0:
-                plt.imshow(np.transpose(input[i], (1, 2, 0)))
-                plt.show()
-                print("loss: ", loss[0])
-                print("label", labels[i])
-                print("predicted: ", predicted[i])
-    return loss[0], correct/total
+
+        if i % 100 == 0:
+            print("loss: ", loss.item())
+            print("label", target[0].item())
+            print("predicted: ", predicted[0].item())
+    return loss.item(), correct/total
 
 def save_checkpoint(state, best_one, filename='rotationnetcheckpoint.pth.tar', filename2='rotationnetmodelbest.pth.tar'):
     torch.save(state, filename)
@@ -66,7 +79,7 @@ def save_checkpoint(state, best_one, filename='rotationnetcheckpoint.pth.tar', f
 def main():
     n_epochs = config["num_epochs"]
     model = RotNet()
-    summary(model, (3, 32, 32))
+    #summary(model, (3, 32, 32))
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=config["learning_rate"], momentum=config["momentum"], weight_decay=config["weight_decay"])
     train_dataset = Data(args.data_dir + "train")
@@ -77,12 +90,12 @@ def main():
     best_loss = 1
     for epoch in range(n_epochs):
      	 #TODO: make your loop which trains and validates. Use the train() func
-         train_loss = train(train_loader, model, criterion, optimizer, config["num_epochs"])
+         train_loss, train_acc = train(train_loader, model, criterion, optimizer, config["num_epochs"])
          val_loss, val_acc = validate(val_loader, model, criterion)
      	 #TODO: Save your checkpoint
          best_one = val_loss < best_loss
          save_checkpoint(model.state_dict(), best_one)
-         print("Epoch %d, Validation Accuracy %d", epoch, val_acc)
+         print("Epoch: %d \nTrain Accuracy: %d\nValidation Accuracy %d" % (epoch, train_acc, val_acc))
 
 if __name__ == "__main__":
     main()
